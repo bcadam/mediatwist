2d2540336b6ba7f9798b6050714c160066ff192b
Adam Cragg <adam2@MacBook-Pro.local>
2015-06-02 19:49:48 -0400

    routing

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', '/public/home.html');
+app.get('/', 'home.html');
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
