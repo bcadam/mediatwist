b6477e60dfa627c945431272fc65f5491b70acfb
Adam Cragg <adam2@MacBook-Pro.local>
2015-06-02 20:14:18 -0400

    routing

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-    res.sendfile('home.html' );
+    res.send('home.html' );
 });
 
 app.listen(app.get('port'), function() {
