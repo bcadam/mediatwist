41f4a8f175e238812c2e9b5dbe05c9c28b21d5a6
Adam Cragg <adam2@MacBook-Pro.local>
2015-06-02 23:01:52 -0400

    fix route

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-//app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', function(req, res) {
-    res.sendfile('public/home.html');
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
 
 app.listen(app.get('port'), function() {
