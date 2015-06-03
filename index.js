00c682daf7d4607ed47f5deba1cef8f41d437ddb
Adam Cragg <adam2@MacBook-Pro.local>
2015-06-02 22:48:01 -0400

    update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-    res.sendFile('/public/home.html' );
+    res.sendFile('home.html' );
 });
 
 app.listen(app.get('port'), function() {
