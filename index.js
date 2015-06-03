57a4c16779ed58c510a819b1ced54a810629a0b3
Adam Cragg <adam2@MacBook-Pro.local>
2015-06-02 23:23:47 -0400

    add library route

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+
 
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/public/home.html');
@@ -11,6 +11,12 @@ app.get('/', function(request, response) {
 app.get('/mediatwist.js', function(request, response) {
   response.sendFile(__dirname + '/public/parse/public/mediatwistreacted.js');
 });
+
+
+
+app.use(express.static(__dirname + '/public'));
+
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
